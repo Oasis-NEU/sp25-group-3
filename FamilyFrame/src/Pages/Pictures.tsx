4318e67006cf9732794d00b183cdb28b2aba6152
DanWong530 <wong.dan@northeastern.edu>
2025-02-02 13:52:18 -0500

    Update Pictures.tsx

---
+++ FamilyFrame/src/Pages/Pictures.tsx
@@ -0,0 +1,11 @@
+function Pictures(){
+    return (
+        <div>
+            <center><h1>Pictures</h1></center>
+            
+            <text>Upload Photos: </text>
+
+        </div>
+    )
+}
+export default Pictures;
