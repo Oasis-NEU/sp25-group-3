80db9d56dba1971087a642c7a17c91ab8bdcd5d9
Jrodrigo06 <jeromerodrigo@Jeromes-MacBook-Air.local>
2025-02-02 13:39:06 -0500

    Updated HomePage

--- FamilyFrame/src/Pages/HomePage.tsx
+++ FamilyFrame/src/Pages/HomePage.tsx
@@ -1,11 +1,10 @@
-import { Link } from 'react-router-dom';
-import AboutYou from './AboutYou';  
+//import { Link } from 'react-router-dom';
+//import AboutYou from './AboutYou';  
 
 function HomePage(){
     return (
-        <div>
-             <Link to="AboutYou" className = "button"> Go To About You </Link>
-        </div>
+        <button 
+  className="button button--large blue" id="AboutYou-button"> Checkout </button>
     )
 }
 
