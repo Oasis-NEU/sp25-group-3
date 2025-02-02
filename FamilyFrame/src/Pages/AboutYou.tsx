0b9df635c2bf1e2e1df672c3e5110e8d55bb9f77
ramanang711 <127068368+ramanang711@users.noreply.github.com>
2025-02-02 13:48:39 -0500

    I went in the ABoutYou tsx folder and added in the X bar

---
+++ FamilyFrame/src/Pages/AboutYou.tsx
@@ -0,0 +1,9 @@
+function AboutYou() {
+    return (
+      <div style={{ fontSize: "64px", color: "red", position: "absolute", top: 0, left: 0 }}>
+        X
+      </div>
+    );
+  }  
+
+export default AboutYou;
